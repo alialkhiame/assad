e4df2606031c2e93de36a3faa567011ee34bdf17
alialkhiame <32980384+alialkhiame@users.noreply.github.com>
2024-08-18 12:18:31 +0200

    folder rename

--- assadWebsite/contact.php
+++ assadWebsite/contact.php
@@ -1,20 +1,24 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+if(!empty($_POST['website'])) die();
 
-    $to = 'moassad@mrweiss.de';
-    $subject = 'New Contact Form Submission';
-    $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
 
-    $headers = "From: $email\r\n";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$name = trim(htmlspecialchars($_POST['name']));
+  $visitor_email = trim(htmlspecialchars($_POST['email']));
+  $message = trim(htmlspecialchars($_POST['msg']));
+	$email_subject = "New Form submission";
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Success";
-    } else {
-        echo "Error";
-    }
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message";
+
+$to="ali@alkhiami.de";
+mail($to,$email_subject,$email_body,$visitor_email);
+$text = "Thank you for your Message, i'll be reaching out to you soon";
+echo '<pre>';
+echo htmlspecialchars($text);
+echo '</pre>';
+
+
+
 }
 ?>
